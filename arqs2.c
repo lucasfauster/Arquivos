3407d52bcee497cc769aeba97641718f892429c1
fauster <lucasfauster@id.uff.br>
2020-10-30 16:41:58 -0300

    questão 2 atualizada

--- arqs2.c
+++ arqs2.c
@@ -17,11 +17,6 @@ FILE * junta_arq(FILE *a, FILE *b){
 void main(){
   FILE *a = fopen("a.bin", "wb+");
   FILE *b = fopen("b.bin", "wb+");
-  char p1[4] = {"prescindir", "diligencia", "intemperie", "corroborar"};
-  char p2[4] = {"incipiente", "referencia", "presuncoso", "intrinseco"};
-  for(int i = 0; i < 4; i++){
-    fwrite(p1[i], sizeof(char), 10, a);
-    fwrite(p2[i], sizeof(char), 10, b);
-  }
+  
   FILE *c = junta_arq(a, b);
 }
