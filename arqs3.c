070b510e502a4e1b89be143735837c930e3be6b5
fauster <lucasfauster@id.uff.br>
2020-10-30 21:37:44 -0300

    questão 3 editada

--- arqs3.c
+++ arqs3.c
@@ -15,18 +15,18 @@ Cliente * consulta(char cpf[11], FILE *f){
   int tam_pulo = (sizeof(int)*2) + sizeof(float) + 40; //distância até o cpf do próximo registro
 
   fread(cpf_atual, sizeof(char), 11, f);
-  while(cpf_atual != cpf && !feof(f)){
+  while(strcmp(cpf_atual,cpf) == 0 && !feof(f)){
     fseek(f, tam_pulo, SEEK_CUR);
     fread(cpf_atual, sizeof(char), 11, f);
   }
 
-  if(cpf_atual  == cpf){
+  if(strcmp(cpf_atual,cpf)){
     Cliente * cli = (Cliente*)malloc(sizeof(Cliente));
     fseek(f, -51, SEEK_CUR); //volta para o campo nome do registro desejado
-    fread(cli->nome, sizeof(char), 40, f);
-    fread(cli->cpf, sizeof(char), 11, f);
-    fread(cli->conta_corrente, sizeof(int), 1, f);
-    fread(cli->agencia, sizeof(int), 1, f);
+    fread(&cli->nome, sizeof(char), 40, f);
+    fread(&cli->cpf, sizeof(char), 11, f);
+    fread(&cli->conta_corrente, sizeof(int), 1, f);
+    fread(&cli->agencia, sizeof(int), 1, f);
     fread((void*)&cli->saldo, sizeof(float), 1, f); // gambiarra para ler floats do arquivo
     return cli;
   }
@@ -36,4 +36,5 @@ Cliente * consulta(char cpf[11], FILE *f){
 void main(){
   FILE *clientes = fopen("clientes.bin", "rb");
   Cliente *c = consulta("12345678901", clientes);
+  fclose(clientes);
 }
